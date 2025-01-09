92924e41dde30951d2571d37f0440ef9a13fc843
arunprakash23797@gmail.com <ArunDhoundiyal>
2025-01-10 00:41:05 +0530

    Edivon backend school payment and transaction assignment

--- dateTime.js
+++ dateTime.js
@@ -2,9 +2,9 @@ const dateTime = () => {
     const date = `${String(new Date().getDate()).padStart(2, "0")}`
     const month = `${String(new Date().getMonth()+1).padStart(2, "0")}`
     const year = `${String(new Date().getFullYear())}`
-    const hours = `${String(new Date().getHours())}`
-    const minute = `${String(new Date().getMinutes())}`
-    const second = `${String(new Date().getSeconds())}`
+    const hours = `${String(new Date().getHours()).padStart(2, "0")}`
+    const minute = `${String(new Date().getMinutes()).padStart(2, "0")}`
+    const second = `${String(new Date().getSeconds()).padStart(2, "0")}`
     return `${year}-${month}-${date} ${hours}:${minute}:${second}`
 }
 
